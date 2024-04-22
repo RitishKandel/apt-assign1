e192296447f30e277d59c8c038b867ca1915186e
Ritish Kandel <kandelritiz@gmail.com>
2024-04-22 13:43:08 +1000

    last change

--- vacuumClean.cpp
+++ vacuumClean.cpp
@@ -1,4 +1,13 @@
-
+/*
+Understanding the given code was crucial in implementing the project. Initial approach was to just get it working before anything else which
+might not have been a great approach if multiple people were working on it. After the project was working, the code was refactored. One of
+biggest issue was keeping the code clean without the use of break and multiple return. Initially, lots of boolean flags outside the loops were used.
+Segregated if statements made life harder. Later on, the code was refactored to use else if statements instead of segregated if statements.
+One of the biggest downside of this approach is that even if a user wants to quit, all the computation for all other command permutations
+are done which is not ideal if performance is a concern.In terms of improvements, the menu system could have been better designed. Instead
+of all the menu items being loaded from the same class, the menu items could be made into their own class. This would promote better
+extensibility in the future.
+*/
 
 #include "game.h"
 #include "helper.h"
